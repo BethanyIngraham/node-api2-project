48ac3de54118df290a03ddd53a8875fb7ebdc2cc
Bethany Ingraham <BethFIngraham@gmail.com>
2024-08-21 07:57:24 -0400

    PUT endpoint implemented

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -53,4 +53,34 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    try{
+        const {title, contents} = req.body
+        if(!title || !contents){
+            res.status(400).json({
+                message: 'Please provide title and contents for the post'
+            })
+        }else{
+            const {id} = req.params
+            const post = await Posts.findById(id)
+            if(!post){
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist'
+                })
+            }else{
+                const numberOfUpdatedPosts = await Posts.update(id, {title, contents})
+                if(numberOfUpdatedPosts){
+                    const {id} = req.params
+                    const updatedPost = await Posts.findById(id)
+                    res.status(200).json(updatedPost)
+                }
+            }
+            }
+    }catch(err){
+        res.status(500).json({
+            message: 'the post information could not be modified'
+        })
+    }
+})
+
 module.exports = router
